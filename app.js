632342c2914e73185efdfa887edea6a10434a55b
Unknown <bartusiuks@gmail.com>
2017-09-08 12:06:51 +0300

    v0.0.2: version up, made WebSocket Communication.

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
+var express = require('express');
+var app = express();
+var serv = require('http').Server(app);
+app.get('/', function(req, res){
+	res.sendFile(__dirname + '/client/index.html')
+});
+app.use('/client', express.static(__dirname + '/client'));
 
-console.log('Hello world');
-var a = 1;
-a = a+1;
-console.log(a);
+serv.listen(2000);
+console.log("Server started");
+
+var io = require('socket.io')(serv,{});
+io.sockets.on('connection', function(socket){
+	console.log('socket connection');
+	
+	socket.on('happy', function(data){
+		console.log('happy because' + data.reason);
+	});
+	
+	socket.emit('serverMsg', {
+		msg:'hello'
+	});
+	
+});
